43e287896610d6d611ad3216ccd59ab129a1f673
ryan2625 <ryancfreas@gmail.com>
2024-04-15 12:47:49 -0500

    Refactor algo

--- src/stringAnimations.js
+++ src/stringAnimations.js
@@ -1,7 +1,7 @@
 let originalTitle = "< Parser.io >".split("")
-let originalSub = "Search your directories for regex".split("")
+let originalSub = "Search your directories with regex".split("")
 let title = "< Parser.io >"
-let sub = "Search your directories for regex"
+let sub = "Search your directories with regex"
 
 const chars = [
     ...Array.from({length: 94}, (_, i) => String.fromCharCode(33 + i)),
@@ -9,10 +9,10 @@ const chars = [
     'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 
     'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 
     'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 
-    'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',"α", "β", "γ", "δ", "ε", "ζ", "η", "θ", "ι", "κ", "λ", "μ", "ν", "ξ", "ο", "π", "ρ", "σ", "τ", "υ", "φ", "χ", "ψ", "ω"
+    'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',"α", "β", "γ", "δ", 
+    "ε", "ζ", "η", "θ", "ι", "κ", "λ", "μ", "ν", "ξ", "ο", "π", "ρ", "σ", 
+    "τ", "υ", "φ", "χ", "ψ", "ω"
 ];
-
-
 
 $(document).ready(function () {
 
@@ -24,51 +24,45 @@ $(document).ready(function () {
         return arr[Math.floor(Math.random() * arr.length)];
     }
 
-    // Surprisingly this algo I came up with works - DON'T TOUCH IT
-    // Will document it later...
+    // Algorithm for the animation that updates and scrambles our titles
     async function titleDisplay() {
+        // How long you want the animation to run for
+        const iterations = 70
+        // How fast the letters change
+        const sleepTime = 37
+        // Delay between changing the main title and sub title
+        const delayBetweenTitles = 5
         let time = 0
-        let iterations = 70
-        let sleepTime = 35
         while (time < iterations) {
             let newTitle = title.split("")
             let newSub = sub.split("")
-
+            // Go through our title characters and decide to either replace it
+            // with a random character or to update it with the original and 
+            // final character (we calculate its distance from the front of
+            // the array and update the string sequentially.)
             for (let char in newTitle) {
-                let status = newTitle.length - char + time 
-                let mainIterations = iterations - newSub.length
+                let status = time + newTitle.length - char 
+                let mainIterations = iterations - newSub.length - delayBetweenTitles
                 if (status >=  mainIterations) {
-                    title = title.split("")
-                    title[char] = originalTitle[char]
                     newTitle[char] = originalTitle[char]
-                    title = title.join("")
-                    $("#main_title").text(title)
-
                 } else {
                 newTitle[char] = getRandomElement(chars)
                 }
             }
-
+            
             for (char in newSub) {
-                let status = newSub.length - char + time 
-                let subIterations = iterations 
-                if (status >=  subIterations) {
-                    sub = sub.split("")
-                    sub[char] = originalSub[char]
+                let status = time + newSub.length - char 
+                if (status >= iterations) {
                     newSub[char] = originalSub[char]
-                    sub = sub.join("")
-                    $("#sub_title").text(sub)
-
                 } else {
                 newSub[char] = getRandomElement(chars)
                 }
             }
-            
             title = newTitle.join("")
             sub = newSub.join("")
             $("#main_title").text(title)
             $("#sub_title").text(sub)
-            time += 1
+            time++
             await sleep(sleepTime)
         }
     }
