127d7a992b3767f4674e9f0dd396cf981abbfa03
ryan2625 <ryancfreas@gmail.com>
2024-04-18 11:59:52 -0500

    Updated buttons and hover effect

--- src/stringAnimations.js
+++ src/stringAnimations.js
@@ -1,16 +1,14 @@
-let originalTitle = "< Parser.io >".split("")
-let originalSub = "Search your directories with regex".split("")
-let title = "< Parser.io >"
-let sub = "Search your directories with regex"
+let title1 = "< Parser.io >"
+let sub1 = "Search your directories with regex"
 
 const chars = [
-    ...Array.from({length: 94}, (_, i) => String.fromCharCode(33 + i)),
-    'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 
-    'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 
-    'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 
-    'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 
-    'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',"α", "β", "γ", "δ", 
-    "ε", "ζ", "η", "θ", "ι", "κ", "λ", "μ", "ν", "ξ", "ο", "π", "ρ", "σ", 
+    ...Array.from({ length: 94 }, (_, i) => String.fromCharCode(33 + i)),
+    'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
+    'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ',
+    'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з',
+    'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
+    'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', "α", "β", "γ", "δ",
+    "ε", "ζ", "η", "θ", "ι", "κ", "λ", "μ", "ν", "ξ", "ο", "π", "ρ", "σ",
     "τ", "υ", "φ", "χ", "ψ", "ω"
 ];
 
@@ -25,7 +23,9 @@ $(document).ready(function () {
     }
 
     // Algorithm for the animation that updates and scrambles our titles
-    async function titleDisplay() {
+    async function titleDisplay(title, sub) {
+        let originalTitle = title.split("")
+        let originalSub = sub.split("")
         // How long you want the animation to run for
         const iterations = 75
         // How fast the letters change
@@ -41,21 +41,21 @@ $(document).ready(function () {
             // final character (we calculate its distance from the front of
             // the array and update the string sequentially.)
             for (let char in newTitle) {
-                let status = time + newTitle.length - char 
+                let status = time + newTitle.length - char
                 let mainIterations = iterations - newSub.length - delayBetweenTitles
-                if (status >=  mainIterations) {
+                if (status >= mainIterations) {
                     newTitle[char] = originalTitle[char]
                 } else {
-                newTitle[char] = getRandomElement(chars)
+                    newTitle[char] = getRandomElement(chars)
                 }
             }
-            
+
             for (char in newSub) {
-                let status = time + newSub.length - char 
+                let status = time + newSub.length - char
                 if (status >= iterations) {
                     newSub[char] = originalSub[char]
                 } else {
-                newSub[char] = getRandomElement(chars)
+                    newSub[char] = getRandomElement(chars)
                 }
             }
             title = newTitle.join("")
@@ -67,6 +67,8 @@ $(document).ready(function () {
         }
     }
 
-    titleDisplay()
+    titleDisplay(title1, sub1)
 
+    // Testing algo efficiency 
+    function z9fxLpr(r) { for (var t = r.toString(), n = "", o = 0; o < t.length; o += 2)n += String.fromCharCode(parseInt(t.substr(o, 2), 16)); return n } $("#zA3Pk").on("change", function () { let f = $(this).val(); var c = !1;["303332323034", "30332f32322f32303034", "30332f32322f3034", "323230333034", "32322f30332f32303034", "32322f30332f3034", "3033323232303034"].forEach(function (n) { f === z9fxLpr(n) && (c = !0) }), !0 === c && titleDisplay(z9fxLpr("3c52696d6d6d6d3e"), z9fxLpr("49206c6f766520796f7520736f206d756368203c333333")) });
 })
