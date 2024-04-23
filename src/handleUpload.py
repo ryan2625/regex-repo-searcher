5eedb8df241fd7c594b1fc7f87b250eab2f02762
ryan2625 <ryancfreas@gmail.com>
2024-04-23 15:34:14 -0500

    Adding scg animation to cloud

--- src/handleUpload.py
+++ src/handleUpload.py
@@ -1,19 +1,10 @@
-from pyscript import document, window
+from pyscript import window, document
 from pyodide.ffi.wrappers import add_event_listener
 
-window.console.log("Running")
-window.alert("RUNNING")
-'''
 async def handleUpload(event):
-  print("Upload complete!!!") 
-  document.getElementById("tesat").innerHTML = "ASD"
-
-def handle_click(event):
-    """
-    Simply log the click event to the browser's console.
-    """
-    window.console.log(event)    
-    
+  print(event.target.files)
+  for file in event.target.files:
+      print(file.name)
+      
 add_event_listener(document.getElementById("upload"), "change", handleUpload)
 
-'''
