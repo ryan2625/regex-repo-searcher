1ab88853aaf45b4880dcf4749bdab5160a3337ce
ryan2625 <ryancfreas@gmail.com>
2024-05-06 09:18:44 -0500

    Adding step 2

--- src/handleUpload.py
+++ src/handleUpload.py
@@ -4,7 +4,10 @@ from pyodide.ffi.wrappers import add_event_listener
 async def handleUpload(event):
   print(event.target.files)
   for file in event.target.files:
-      print(file.name)
+      #print(file.name)
+      print()
+  document.querySelector(".step-1b").innerText= "Folder Uploaded!"
+  
       
 add_event_listener(document.getElementById("upload"), "change", handleUpload)
 
