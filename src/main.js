5c8983f604fcd223410d07834555429f8f758392
ryan2625 <ryancfreas@gmail.com>
2024-05-13 15:04:14 -0500

    Changing vanilla js to JQUERY

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
     })
 
     $("#upload").click(function () {
-        //Hide the lag of loading the python scripts behind the folder upload prompt
+        //Hide lag of loading the python scripts behind the folder upload prompt
         if (uploadClicked == false) {
             html = '<script type="py" src="./handleParse.py" config="./pyscript.json" defer></script>'
             document.body.insertAdjacentHTML("beforeend", html)
@@ -42,12 +42,12 @@ $(document).ready(function () {
     })
 
     $("#upload").on("input", async function () {
-        document.querySelector(".step-1b").innerText= "Folder Uploaded!"
-        ele = document.querySelector(".step2")
-        ele.classList.add("step-show")
+        $(".step-1b").text("Folder Uploaded!")
+        ele = $(".step2")
+        ele.addClass("step-show")
         await sleep(500)
-        ele = document.querySelector(".bridge2")
-        ele.classList.add("bridge2a")
+        ele = $(".bridge2")
+        ele.addClass("bridge2a")
         $("#upload").off("input")
     })
 
