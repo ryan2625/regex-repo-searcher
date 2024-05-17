47b5f531a2ec421e24149be2a593b2dae8b7e399
ryan2625 <ryancfreas@gmail.com>
2024-05-17 11:58:42 -0500

    Refactor and add logic to CMD commands

--- src/commandLine.js
+++ src/commandLine.js
@@ -22,11 +22,10 @@ $(document).ready(function () {
     with how the letters line up and the scaling is not
     completely even. We could not simply use width:fit content
     or text-align to fix this situation. To ensure characters 
-    line up, add an extra pixel every 3 iterations and change the
+    line up, add 8/9 pixels depending on count and change the
     width of the parent div instead of the input directly. 
     */
-   var listener =  function (e) {
-        console.log("ASD")
+    var listener = function (e) {
         // Detect if user is deleting or adding character to input
         if ($(this).val().length > prevLen) {
             if (count % 5 == 0) {
@@ -48,18 +47,33 @@ $(document).ready(function () {
     $(".prompt-body").on("keypress", "input", function (e) {
         if (e.which == 13) {
             var command = $(this).val().trim().toLowerCase().replace(/\s+/g, ' ')
-            if (((command.substring(0, 7) == "op add " ||
-                command.substring(0, 10) == "op delete " ||
-                command.substring(0, 8) == "op list ") && command.split(" ").length == 3)
-                ||
-                ((command.substring(0, 11) == "op replace ") && command.split(" ").length == 4)) {
-                    $(this).parent().removeClass("inner")
-                    html = `<span>"${$(this).val()}" command successful. Lovely.<span/></br></br>
-                            <div class="line-cmd">C:\WINDOWS\system32>ENTER COMMAND:
-                            <div class="inner"><input type="text" name="" id="cmd-input" autocomplete="off"  class="input-enter"></div>`
-                    $(".prompt-body").append(html)
-            } else {
-                $(this).parent().removeClass("inner")
+            splitCommand = command.split(" ")
+            $(this).parent().removeClass("inner")
+            if ((command.substring(0, 7) == "op add ") && splitCommand.length == 3) {
+                html = `<span>Added "${splitCommand[2]}" to list of search terms. Lovely.<span/></br></br>
+                        <div class="line-cmd">C:\WINDOWS\system32>ENTER COMMAND:
+                        <div class="inner"><input type="text" name="" id="cmd-input" autocomplete="off"  class="input-enter"></div>`
+                $(".prompt-body").append(html)
+            }
+            else if ((command.substring(0, 10) == "op delete ") && splitCommand.length == 3) {
+                html = `<span>Deleted "${splitCommand[2]}" command successful. Lovely.<span/></br></br>
+                        <div class="line-cmd">C:\WINDOWS\system32>ENTER COMMAND:
+                        <div class="inner"><input type="text" name="" id="cmd-input" autocomplete="off"  class="input-enter"></div>`
+                $(".prompt-body").append(html)
+            }
+            else if ((command.substring(0, 11) == "op replace ") && splitCommand.length == 4) {
+                html = `<span>"${$(this).val()}" command successful. Lovely.<span/></br></br>
+                        <div class="line-cmd">C:\WINDOWS\system32>ENTER COMMAND:
+                        <div class="inner"><input type="text" name="" id="cmd-input" autocomplete="off"  class="input-enter"></div>`
+                $(".prompt-body").append(html)
+            }
+            else if ((command.substring(0, 8) == "op list ") && splitCommand.length == 2) {
+                html = `<span>"${$(this).val()}" command successful. Lovely.<span/></br></br>
+                        <div class="line-cmd">C:\WINDOWS\system32>ENTER COMMAND:
+                        <div class="inner"><input type="text" name="" id="cmd-input" autocomplete="off"  class="input-enter"></div>`
+                $(".prompt-body").append(html)
+            }
+            else {
                 html = `<span>Invalid operation. ${$(this).val()} is not recognized as an internal or </br>external command,
                 operable program or batch file.<span/></br></br>
                         <div class="line-cmd">C:\WINDOWS\system32>ENTER COMMAND:
@@ -68,13 +82,14 @@ $(document).ready(function () {
             }
             prevLen = 0
             count = 0
-            lenStack = [0]            
+            lenStack = [0]
             $(this).off("input", listener)
             $(".input-enter").on("input", listener)
             $(".input-enter").focus()
             $(this).prop("disabled", true)
         }
     })
+
 
     $(".input-enter").on("input", listener)
 
